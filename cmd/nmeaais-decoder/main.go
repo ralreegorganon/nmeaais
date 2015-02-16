dc040f239f3331ea0eaafec06efc2451681079e7
Jason Jones <jasonedwardjones@gmail.com>
2015-02-16 13:19:50 -0900

    Add type 5

--- cmd/nmeaais-decoder/main.go
+++ cmd/nmeaais-decoder/main.go
@@ -60,6 +60,17 @@ func main() {
 				}
 				output <- x
 				break
+			case 5:
+				x, err := m.GetAsStaticAndVoyageRelatedData()
+				if err != nil {
+					log.WithFields(log.Fields{
+						"err":     err,
+						"message": m,
+					}).Warn("Couldn't get specific message type")
+					break
+				}
+				output <- x
+				break
 			case 21:
 				x, err := m.GetAsAidToNavigationReport()
 				if err != nil {
